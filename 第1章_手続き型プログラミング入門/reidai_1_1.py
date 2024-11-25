14ccbe096968e7033246dd557c9b4f884d9c81ce
Takeo Yamazaki <cax68080@pop06.odn.ne.jp>
2024-11-25 14:48:16 +0900

    2024/11/25

---
+++ 第1章_手続き型プログラミング入門/reidai_1_1.py
@@ -0,0 +1,31 @@
+def printbk(bk4,bk7):
+    print("bk4:",bk4,"L ","bk7:",bk7,"L")
+    
+bk4 = 0
+bk7 = 0
+printbk(bk4,bk7)
+bk4 += 4
+printbk(bk4,bk7)
+bk7 += bk4
+bk4 = 0
+printbk(bk4,bk7)
+bk4 += 4
+printbk(bk4,bk7)
+bk7 += bk4 - 1
+bk4 -= 3
+printbk(bk4,bk7)
+bk7 = 0
+printbk(bk4,bk7)
+bk7 += bk4
+bk4 = 0
+printbk(bk4,bk7)
+bk4 += 4
+printbk(bk4,bk7)
+bk7 += bk4
+bk4 = 0
+printbk(bk4,bk7)
+
+
+
+
+
