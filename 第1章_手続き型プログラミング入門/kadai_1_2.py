e7a7e3b68b7f6ab84960d35fd78629fbade5e0e9
Takeo Yamazaki <cax68080@pop06.odn.ne.jp>
2024-11-25 18:35:32 +0900

    2024/11/25

---
+++ 第1章_手続き型プログラミング入門/kadai_1_2.py
@@ -0,0 +1,15 @@
+total = 1+2+3+4+5+6+7+8
+x = 43200 / total
+print("1L:",x,"円")
+print("2L:",x*2,"円")
+print("3L:",x*3,"円")
+print("4L:",x*4,"円")
+print("5L:",x*5,"円")
+print("6L:",x*6,"円")
+print("7L:",x*7,"円")
+print("8L:",x*8,"円")
+
+
+
+
+
